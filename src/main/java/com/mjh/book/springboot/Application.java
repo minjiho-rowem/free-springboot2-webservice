39fea9e0024a3f8502791423c1a2e2cb003a554c
minjiho-rowem <mjh0907@rowem.com>
2023-03-03 18:57:07 +0900

    posts-save.mustache 전까지2!

---
+++ src/main/java/com/mjh/book/springboot/Application.java
@@ -0,0 +1,14 @@
+package com.mjh.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		// run 매서드를 통해 내장 WAS 실행 가능
+		SpringApplication.run(Application.class, args);
+	}
+}
